66f4c69846f7cc9256d4b70f3b4d715426ddb04e
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2024-03-28 16:18:44 +0300

    Added the client

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+//import {Link}  from 'react-navigation/native';
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Donut diaries</h1>
+        <div className='landingPage'>
+            <h2>Welcome to Donut dairies!</h2>
+            <h3>We are committed to ensuring your donut cravings attended to without any hitches whatsoever.</h3>
+            <h1>Login</h1>
+            <h1>Sign up</h1>
+        </div>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
